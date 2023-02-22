de25bec58af2fa5ffe90151d5b1c7d9bc3fbfa99
xlimitan <xlimitan@yandex.ru>
2023-02-22 13:49:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        // Пишем код для задачи 1
+    }
+}
