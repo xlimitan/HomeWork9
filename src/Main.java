bb0e119429aedcbed4d7136e53e0629e0d65e571
xlimitan <xlimitan@yandex.ru>
2023-02-22 14:47:51 +0300

    Задача № 1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,21 @@ public class Main {
     public static void main(String[] args) {
         task1();
     }
-
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
     public static void task1() {
         System.out.println("Задача 1");
-        // Пишем код для задачи 1
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int element : arr) {
+            sum += element;
+        }
+        System.out.println(sum);
     }
 }
